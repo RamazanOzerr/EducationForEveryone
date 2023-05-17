a69e72d30bce460f558e58684e78168c439133bc
Ramazan Özer <ozerramazan991@gmail.com>
2023-05-17 21:45:55 +0300

    recent chats list added

---
+++ app/src/main/java/com/example/educationforeveryone/ui/message/ChatRepository.java
@@ -0,0 +1,104 @@
+package com.example.educationforeveryone.ui.message;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.educationforeveryone.ui.PrivateChat.MessageModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatRepository {
+
+    DatabaseReference reference, reference2;
+    private MutableLiveData<List<ChatModel>> liveData;
+    private List<ChatModel> chatModelList;
+    FirebaseUser user;
+    private List<String> userKeyList;
+
+    public ChatRepository(){
+        chatModelList = new ArrayList<>();
+        liveData = new MutableLiveData<>();
+//        reference = FirebaseDatabase.getInstance().getReference("Messages");
+//        reference2 = FirebaseDatabase.getInstance().getReference("Users");
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        userKeyList = new ArrayList<>();
+    }
+
+    public MutableLiveData<List<ChatModel>> getChats(){
+
+//        ValueEventListener valueEventListener = new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+//                    userKeyList.add(dataSnapshot.getKey());
+//
+////                    getUsername();
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        };
+//
+//        Query query = reference.child("fatih"); //todo: user.getUid()
+//        query.addListenerForSingleValueEvent(valueEventListener);
+
+        //todo: db den veri indirme kısmını yazcaz
+
+        ChatModel chatModel = new ChatModel("Azim","fatih");
+        ChatModel chatModel2 = new ChatModel("Ramazan","fatih");
+        ChatModel chatModel3 = new ChatModel("Fatih terim","fatih");
+        ChatModel chatModel1 = new ChatModel("üçlü priz", "fatih");
+
+        chatModelList.add(chatModel);
+        chatModelList.add(chatModel2);
+        chatModelList.add(chatModel3);
+        chatModelList.add(chatModel);
+        chatModelList.add(chatModel2);
+        chatModelList.add(chatModel1);
+        chatModelList.add(chatModel3);
+        chatModelList.add(chatModel);
+        chatModelList.add(chatModel2);
+        chatModelList.add(chatModel3);
+        chatModelList.add(chatModel);
+        chatModelList.add(chatModel2);
+        chatModelList.add(chatModel3);
+        chatModelList.add(chatModel);
+        chatModelList.add(chatModel2);
+        chatModelList.add(chatModel3);
+        chatModelList.add(chatModel);
+        chatModelList.add(chatModel2);
+        chatModelList.add(chatModel3);
+
+        liveData.setValue(chatModelList);
+
+        return liveData;
+    }
+
+//    private void getUsername(){
+//        ValueEventListener valueEventListener = new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        };
+//
+//        Query query = reference2.child()
+//    }
+}
---
+++ app/src/main/java/com/example/educationforeveryone/ui/message/ChatsViewModel.java
@@ -0,0 +1,24 @@
+package com.example.educationforeveryone.ui.message;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatsViewModel extends ViewModel {
+
+    private final MutableLiveData<List<ChatModel>> liveData;
+    private final List<ChatModel> chatModelList;
+
+    public ChatsViewModel() {
+        chatModelList = new ArrayList<>();
+        ChatRepository repository = new ChatRepository();
+        liveData = repository.getChats();
+    }
+
+    public MutableLiveData<List<ChatModel>> getChats(){
+        return liveData;
+    }
+}
